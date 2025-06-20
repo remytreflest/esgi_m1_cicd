6c6bba5184348746132abd16a31699f342133174
treflest remy <mrtreflestremy@outlook.com>
2025-06-20 16:20:43 +0200

    feat(ajout serveur playwright)

--- playwright.config.js
+++ playwright.config.js
@@ -3,4 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   testMatch: '**/*.spec.js',
+  webServer: {
+    command: 'npm start',      // ou 'node index.js'
+    port: 5000,
+    timeout: 30 * 1000,        // max 30s pour démarrer
+    reuseExistingServer: !process.env.CI, // en local, ne redémarre pas si déjà lancé
+  },
 });
