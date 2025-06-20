c44da52d52c8a027f6de715a94f22eb91c549079
treflest remy <mrtreflestremy@outlook.com>
2025-06-20 15:45:26 +0200

    feat(initial)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # esgi_m1_cicd
+
+![CI Status](https://github.com/remytreflest/esgi_m1_cicd/actions/workflows/ci.yml/badge.svg)
---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Projet CI/CD M1 Architecture des logiciels");
+
+const http = require('http');
+
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+  // Réponse HTML
+  const html = `
+    <!DOCTYPE html>
+    <html lang="fr">
+      <head>
+        <meta charset="UTF-8">
+        <title>CICD</title>
+      </head>
+      <body>
+        <h1>Bienvenue sur la page CICD 🚀</h1>
+      </body>
+    </html>
+  `;
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
+});
+
+server.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
